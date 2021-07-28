1e0d218cfb993cc5f390be2001319a52dac2d53e
Jefferson Candido <jefferson.ufu@gmail.com>
2021-07-28 19:41:32 -0300

    Typecasting request

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -2,13 +2,17 @@ import { Request, Response } from "express";
 
 import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
 
+interface IRequest {
+  user_id: string;
+}
+
 class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
     const { user_id } = request.headers;
 
-    const users = this.listAllUsersUseCase.execute({ user_id });
+    const users = this.listAllUsersUseCase.execute({ user_id } as IRequest);
 
     return response.json(users);
   }
